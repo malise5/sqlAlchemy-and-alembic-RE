f42c9cef81bdd81452d882bb4bd36b15e4672fea
Halkano Malise <kudezx@gmail.com>
2023-07-09 17:42:56 +0300

    update

---
+++ app.py
@@ -0,0 +1,17 @@
+# 🤪  setUp Imports
+
+from flask import Flask, jsonify, make_response, request
+from flask_migrate import Migrate
+
+from models import db, Production
+
+# 🤪  Intialize the App
+app = Flask(__name__)
+
+# 🤪  setUp database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# 🤪  setUp migration
+migrate = Migrate(app, db)
+db.init_app(app)
